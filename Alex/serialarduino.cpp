a0227b67ec9fec510c4422faac608d82ad5808b5
shxr3f <e0540641@u.nus.edu>
2021-04-05 19:46:01 +0800

    Updated Serial BareMetal in comments

--- Alex/serialarduino.cpp
+++ Alex/serialarduino.cpp
@@ -9,49 +9,64 @@
 #include "packet.h"
 #include <Arduino.h>
 
-//TBuffer _xmitBuffer[MAX_STR_LEN];
-
-
-char dataSend;
 
 void setupSerial()
 {
-  /*
-	UCSR0C = 0b00000110; //Set to 8N1
-	UBRR0L = 103; //b = 103 for 9600 bps
-	UBRR0H = 0;
-	UCSR0A = 0;
- */
  Serial.begin(9600);
 }
 
 void startSerial()
 {
-//	UCSR0B = 0b00011000;
+
 }
 
 int readSerial(char *buffer)
 {
 
-	int count = 0; 
-	/*
-	while( (UCSR0A & (1 << 7)) == 0)
-	{
-		buffer[count++] = UDR0;
-	}
- */
- while (Serial.available())
- {
-  buffer[count ++] = Serial.read();
- }
+  int count = 0; 
+	
+  while (Serial.available())
+  {
+    buffer[count ++] = Serial.read();
+  }
 	return count;
 
 }
+
+void writeSerial(const unsigned char *buffer, int len)
+{
+  Serial.write(buffer, len);
+}
+
 /*
+ * Uncomment to use baremetal serial
+ * 
+ * 
+
+void setupSerial()
+{
+  UCSR0C = 0b00000110; //Set to 8N1
+  UBRR0L = 103; //b = 103 for 9600 bps
+  UBRR0H = 0;
+  UCSR0A = 0;
+}
+
+void startSerial()
+{
+  UCSR0B = 0b10011000;
+}
+
+ISR(USART_RX_vect)
+{
+  unsigned char data = UDR0;
+
+  writeBuffer(&_recvBuffer,data);
+}
+
 ISR(USART_UDRE_vect)
 {
   unsigned char data;
- TResult result = readBuffer(&_xmitBuffer, &data);
+  TBufferResult result = readBuffer(&_xmitBuffer, &data);
  
   if (result == BUFFER_OK) 
     UDR0A = data;
@@ -64,7 +79,23 @@ ISR(USART_UDRE_vect)
    }
 }
 
-void writeSerial(const char *buffer, int len)
+int readSerial(char *buffer)
+{
+  int count = 0;
+
+  TBufferResult result;
+  do
+  {
+    result = readBuffer(&_recvBuffer, &buffer[count]);
+
+    if(result == BUFFER_OK)
+    {
+      count++
+    }
+  }while(result == BUFFER_OK);
+}
+
+void writeSerial(const unsigned char *buffer, int len)
 {
   TBufferResult result = BUFFER_OK;
 	for(int i = 1; i < len && (result == BUFFER_OK); i++)
@@ -75,8 +106,3 @@ void writeSerial(const char *buffer, int len)
  UCSR0B  |= UDRIEMASK;
 }
 */
-
-void writeSerial(const unsigned char *buffer, int len)
-{
-  Serial.write(buffer, len);
-}
